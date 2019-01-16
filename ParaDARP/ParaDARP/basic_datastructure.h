f8e89865f0185369ba81fa4a603e442e20f609b3
henhimicons <kanberra354562007@gmail.com>
2019-01-16 14:49:52 +0800

    add declaration of Point and Request

--- ParaDARP/ParaDARP/basic_datastructure.h
+++ ParaDARP/ParaDARP/basic_datastructure.h
@@ -1,4 +1,14 @@
 #pragma once
+struct Point {
+	int id;
+	double x;
+	double y;
+	int service_time;
+	int quality;
+	int time_window_start;
+	int time_window_end;
+};
 struct request{
-	
+	Point pickup;
+	Point delivery;
 };
